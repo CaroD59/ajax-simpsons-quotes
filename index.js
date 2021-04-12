009abb6b5162a908750e2b92e73f2effe08caa6e
CaroD59 <dorchies.c@gmail.com>
2021-04-12 19:29:05 +0200

    Axios work finished

---
+++ index.js
@@ -0,0 +1,14 @@
+axios.get("https://simpsons-quotes-api.herokuapp.com/quotes")
+.then(({data}) => {
+    console.log(data[0]);
+    const quote = data[0];
+    const myHtml = `
+        <article>
+            <h3>${quote.character}</h3>
+            <img src="${quote.image}" />
+            <p>${quote.quote}</p>
+        <article>
+    `;
+    const myDiv = document.querySelector("#simpson-quote");
+    myDiv.innerHTML = myHtml;
+})
